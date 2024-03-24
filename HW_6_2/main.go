c3b257152f4bd27a73815a2ca92592b4f3bba018
unknown <schmidtik4@gmail.com>
2024-03-24 02:58:29 +0200

    fix

--- HW_6_2/main.go
+++ HW_6_2/main.go
@@ -6,44 +6,51 @@ import (
 )
 
 func Publisher(ch chan<- int, wg *sync.WaitGroup) {
-	defer close(ch)
 	defer wg.Done()
 	for i := 1; i <= 10; i++ {
-		ch <- i * 2
+		ch <- i
 	}
+	close(ch)
+
 }
 
 func ChanelBroadkaster(ch <-chan int, chSl []chan int, n int) {
 	for i := range ch {
 		for a := 0; a < n; a++ {
-			val := i // Створюємо копію значення i
-			chSl[a] <- val
+			chSl[a] <- i
 		}
 	}
+	for a := 0; a < n; a++ {
+		close(chSl[a])
+	}
+
 }
 
-func Sucscriber(ch <-chan int, id int, wg *sync.WaitGroup) {
+func Sucscriber(chSl []chan int, n int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := range ch {
-		fmt.Printf("Receiver #%d: %d\n", id, i)
+	for i := range chSl[n] {
+		fmt.Printf("Receiver #%d: %d\n", n, i)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
-	n := 10
+	n := 3
 	chanelMain := make(chan int)
 	chanelsSlice := make([]chan int, n)
+	for i := range chanelsSlice {
+		chanelsSlice[i] = make(chan int)
+	}
 
 	go ChanelBroadkaster(chanelMain, chanelsSlice, n)
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go Sucscriber(chanelsSlice[i], i, &wg)
+		go Sucscriber(chanelsSlice, i, &wg)
 	}
 
 	wg.Add(1)
 	go Publisher(chanelMain, &wg)
-
 	wg.Wait()
+
 }
